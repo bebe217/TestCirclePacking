7fbf9786c6d309b291bd828f7342743d29fb6aa3
bebeda <bebe217@naver.com>
2025-05-01 21:09:41 +0900

    Initial Commit

---
+++ TestCirclePacking/TestCirclePackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCirclePackingApp.swift
+//  TestCirclePacking
+//
+//  Created by bebe on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TestCirclePackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
